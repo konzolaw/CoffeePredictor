71aa3b40c47b63fefbc40f64daa76f335ddfcbd6
konzolaw <iykekonzolaw21@gmail.com>
2025-05-02 02:15:29 +0300

    coffeee predict

--- coffeeApp/settings.py
+++ coffeeApp/settings.py
@@ -136,4 +136,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='iykekonzolaw21@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', DEFAULT_AUTO_FIELD='timber')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='timber')
